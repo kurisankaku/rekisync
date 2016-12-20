bfd806dd736d8aa60a0adc2ce397429e0017eae1
Kuriyama Munehisa <kurisankaku@gmail.com>
2016-12-20 15:48:32 +0900

    Set initialize sidekiq worker for test.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'sidekiq/testing'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -25,6 +26,9 @@ require 'rspec/rails'
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
+# A test fake that pushes all jobs into a jobs array.
+Sidekiq::Testing.fake!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -58,6 +62,8 @@ RSpec.configure do |config|
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.start
+
+    Sidekiq::Worker.clear_all
 
     # Ensure default time zone.
     Time.zone = "UTC"
