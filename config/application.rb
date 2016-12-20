5ad1435b0585bb9c418df9b8ac9da076c3fd5632
Kuriyama Munehisa <kurisankaku@gmail.com>
2016-12-20 16:19:27 +0900

    Config active job queue adapter.

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Rekisync
     # Set default time zone.
     config.time_zone = 'UTC'
     config.active_record.default_timezone = :utc
+
+    # Set active job queue adapter.
+    config.active_job.queue_adapter = :sidekiq
   end
 end
