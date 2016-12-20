56e46d62c6b2ae761f51efa0ad2dca76a4374859
Kuriyama Munehisa <kurisankaku@gmail.com>
2016-12-20 18:44:42 +0900

    Create login and signup routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :login, only: [:index, :create]
+  resources :sign_up, only: [:index, :create]
 end
