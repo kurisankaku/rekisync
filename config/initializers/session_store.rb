38d6e2b738e3f53979ae91ffcc102355c7069d14
Kuriyama Munehisa <kurisankaku@gmail.com>
2016-12-21 15:26:24 +0900

    Manage session by redis.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_rekisync_session", expire_after: 1.month
+# Rails.application.config.session_store :cookie_store, key: "_rekisync_session", expire_after: 1.month
+Rails.application.config.session_store :redis_store, servers: "redis://192.168.33.10:6379/1", key: "lgewvnphasket", expire_after: 1.month
