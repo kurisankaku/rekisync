1a35893f971a860e5b0f20eeacc510ac672cf5e8
Kuriyama Munehisa <kurisankaku@gmail.com>
2017-01-04 18:52:25 +0900

    Fix twitter secret key management.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  # provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :twitter, "eHlj7MFloX5YI8jNFBPXKGLqf", "dTQdsO8Tv9CZZv3TuTEDGdydx0AuLELKBDH8NWd9kYQRALBJv8"
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
