ad45b1f8c91cd36bf108dcb2c7b488e217e921f4
kurisankaku <kurisankaku@gmail.com>
2017-02-07 00:03:44 +0900

    Remove locale from routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,21 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /#{I18n.available_locales.map(&:to_s).join("|")}/ do
-    root to: "home#index"
+  root to: "home#index"
 
-    resources :login, only: [:index, :create]
-    delete "logout" => "login#destroy", as: "logout"
+  resources :login, only: [:index, :create]
+  delete "logout" => "login#destroy", as: "logout"
 
-    resources :sign_up, only: [:index, :create]
-    resources :confirm_email, only: [:show, :index] do
-      collection do
-        get "resend" => :resend, as: "resend"
-        put "update_email" => :update_email, as: "update_email"
-      end
+  resources :sign_up, only: [:index, :create]
+  resources :confirm_email, only: [:show, :index] do
+    collection do
+      get "resend" => :resend, as: "resend"
+      put "update_email" => :update_email, as: "update_email"
     end
+  end
 
-    namespace :settings do
-      resource :account, only: [:show, :update, :destroy]
-      resource :emails, only: [:show, :update]
-      resource :profile, except: :destroy
-    end
+  namespace :settings do
+    resource :account, only: [:show, :update, :destroy]
+    resource :emails, only: [:show, :update]
+    resource :profile, except: :destroy
   end
 
   namespace :api do
