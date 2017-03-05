37724dc5445456b731d11c8a80c4c19c5c4a7ca2
kurisankaku <kurisankaku@gmail.com>
2017-03-06 01:33:41 +0900

    Modify user follow api routes.

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index] do
-        resources :followings, only: [:index, :create, :destroy], module: :users
+        resources :followings, only: [:index], module: :users
         resources :followers, only: [:index], module: :users
+        collection do
+          resources :followings, only: [:create, :destroy], module: :users
+        end
       end
     end
   end
