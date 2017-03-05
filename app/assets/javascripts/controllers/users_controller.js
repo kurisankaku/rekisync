9e4aa39f4ba7d6e35c66b5da1e087adc302bc31b
kurisankaku <kurisankaku@gmail.com>
2017-03-06 01:34:12 +0900

    Modify user_controller.js

--- app/assets/javascripts/controllers/users_controller.js
+++ app/assets/javascripts/controllers/users_controller.js
@@ -1,20 +1,53 @@
-function followUser(name) {
-  request.post('/api/v1/users/followings')
-    .send({ name: name })
-    .type('json')
-    .end(function(err, res) {
-      if (err || !res.ok) {
-        console.log('err!!!');
-      }
+var userFollows = {};
+var request = window.superagent;
+
+function followUser(element, name, isFollow) {
+  if ((userFollows[name] && userFollows[name].isFollow) || isFollow) {
+    userFollows[name] = false;
+    unfollowingUser(name)
+      .then()
+      .catch(function(err) {
+        userFollows[name] = true;
+      });
+  } else {
+    userFollows[name] = true;
+    followingUser(name)
+      .then()
+      .catch(function(err) {
+        userFollows[name] = false;
+      });
+  }
+}
+
+function followingUser(name) {
+  return new Promise(
+    function (resolve, reject) {
+      request.post('/api/v1/users/followings')
+        .send({ name: name })
+        .type('json')
+        .end(function(err, res) {
+          if (err || !res.ok) {
+            console.log('err!!!');
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
     });
 }
 
-function unfollowUser(name) {
-  request.del('/api/v1/users/followings')
-    .type('json')
-    .end(function(err, res) {
-      if (err || !res.ok) {
-        console.log('err!!!');
-      }
+function unfollowingUser(name) {
+  return new Promise(
+    function (resolve, reject) {
+      request.del('/api/v1/users/followings')
+        .type('json')
+        .end(function(err, res) {
+          if (err || !res.ok) {
+            console.log('err!!!');
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
     });
 }
