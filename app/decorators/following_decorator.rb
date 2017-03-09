9fead7324cab28f144248a888428cfa24d255097
kurisankaku <kurisankaku@gmail.com>
2017-03-09 23:09:05 +0900

    Fix followers bug.

--- app/decorators/following_decorator.rb
+++ app/decorators/following_decorator.rb
@@ -1,6 +1,7 @@
 class FollowingDecorator < Draper::Decorator
   delegate_all
   decorates_association :user
+  decorates_association :owner, with: UserDecorator
 
   def self.collection_decorator_class
     PaginatingDecorator
